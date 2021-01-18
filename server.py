182cfa0f647d5bef63a75fa3abe12a097e8ee5c2
soorya <soorya.ns85@gmail.com>
2021-01-18 15:05:43 +0530

    Converted Simple html app to Pyton app

--- server.py
+++ server.py
@@ -1,17 +1,12 @@
 # serve.py
 
-from flask import Flask
-from flask import render_template
-
-# creates a Flask application, named app
+from flask import Flask, render_template
 app = Flask(__name__)
-
-# a route where we will display a welcome message via an HTML template
+ 
 @app.route("/")
-def hello():
-    message = "Hello, World"
-    return render_template('index.html', message=message)
-
-# run the application
+def main():
+    return render_template('index.html')
+ 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+ 
